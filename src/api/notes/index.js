30eb740e782c9bcef5f9e12c925dd50579100331
Hadi Rahmah Esa Putra <hadiesarahma@gmail.com>
2023-03-18 20:17:55 +0700

    Initial commit

---
+++ src/api/notes/index.js
@@ -0,0 +1,12 @@
+//untuk plugin akan disimpan pada file ini
+const NotesHandler = require('./handler');
+const routes = require('./routes');
+ 
+module.exports = {
+  name: 'notes',
+  version: '1.0.0',
+  register: async (server, { service }) => {
+    const notesHandler = new NotesHandler(service);
+    server.route(routes(notesHandler));
+  },
+};
