f0667d8a9527252c649e0294ad72caff6614e86b
Hadi Rahmah Esa Putra <hadiesarahma@gmail.com>
2023-03-28 20:09:07 +0700

    Pembaharuan

--- src/api/notes/routes.js
+++ src/api/notes/routes.js
@@ -1,31 +1,29 @@
-//untuk masalah routing akan disimpan di dalam file ini
 const routes = (handler) => [
-    {
-        method: 'POST',
-        path: '/notes',
-        handler: handler.postNoteHandler,
-    },
-    {
-        method: 'GET',
-        path: '/notes',
-        handler: handler.getNotesHandler,
-    },
-    {
-        method: 'GET',
-        path: '/notes/{id}',
-        handler: handler.getNoteByIdHandler,
-    },
-    {
-        method: 'PUT',
-        path: '/notes/{id}',
-        handler: handler.putNoteByIdHandler,
-    },
-    {
-        method: 'DELETE',
-        path: '/notes/{id}',
-        handler: handler.deleteNoteByIdHandler,
-    },
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: handler.postNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: handler.getNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: handler.getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: handler.putNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: handler.deleteNoteByIdHandler,
+  },
 ];
 
-//untuk mengimport agar bisa digunakan server.js (kelas lain)
 module.exports = routes;
